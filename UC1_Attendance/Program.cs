f7570995c6a83dbbf78f7e9f5b7cdc317d6243b2
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-09-29 23:50:25 +0530

    UC1

---
+++ UC1_Attendance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage compuation program");
+            int isPresent = Attendance();
+            if (isPresent == 0)
+                Console.WriteLine("Employee is Absent!");
+            else
+                Console.WriteLine("Employee is Present!");
+        }
+
+        static int Attendance()
+        {
+            Random random= new Random();
+            return random.Next(0, 2);
+        }
+    }
+}
