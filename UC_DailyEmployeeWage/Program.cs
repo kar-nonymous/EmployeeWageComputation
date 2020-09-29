f9584ad0673533871fb5b836c3a9a035b4d8bb37
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-09-29 23:59:18 +0530

    UC2

---
+++ UC_DailyEmployeeWage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage compuation program");
+            int present = Attendance();
+            int dailyEmployeeWage = 0;
+            if (present == 0)
+                Console.WriteLine("Employee is Absent");
+            else
+                Console.WriteLine("Employee is Present");
+            if (present == 1)
+            {
+                dailyEmployeeWage = DailyEmployeeWage();
+            }
+        }
+
+        static int Attendance()
+        {
+            Random random = new Random();
+            return random.Next(0, 2);
+        }
+        static int DailyEmployeeWage()
+        {
+            int fullDayHour = 8;
+            int perHourWage = 20;
+            return fullDayHour * perHourWage;
+        }
+    }
+}
