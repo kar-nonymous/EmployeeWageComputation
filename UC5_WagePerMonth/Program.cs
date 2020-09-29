42454cbdf4542ed821ceebdbbdc9fd1414806597
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-09-30 00:04:21 +0530

    UC5

---
+++ UC5_WagePerMonth/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UC5_WagePerMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
