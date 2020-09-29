eb71815670294ea2fcb2d3b09a12ef2d4bdee4a9
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-09-30 00:01:03 +0530

    UC3

---
+++ UC3_PartTimeWage/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage compuation program");
+            int present = Attendance();
+            int dailyEmployeeWage = 0;
+            int partTimeWage = 0;
+            if (present == 0)
+                Console.WriteLine("Employee is Absent");
+            else
+                Console.WriteLine("Employee is Present");
+            if (present == 1)
+            {
+                dailyEmployeeWage = DailyEmployeeWage();
+                int partTime = Attendance();
+                if (partTime == 1)
+                    partTimeWage = PartTimeWage();
+            }
+            Console.WriteLine("Daily Employee Wage : {0}", dailyEmployeeWage);
+            Console.WriteLine("Part Time Employee Wage : {0}", partTimeWage);
+        }
+
+        static int Attendance()
+        {
+            Random random = new Random();
+            return random.Next(0, 2);
+        }
+        static int DailyEmployeeWage()
+        {
+            int fullDayHour = 8;
+            int perHourWage = 20;
+            return fullDayHour * perHourWage;
+        }
+        static int PartTimeWage()
+        {
+            int partTimeHour = 4;
+            int perHourWage = 20;
+            return partTimeHour * perHourWage;
+        }
+    }
+}
